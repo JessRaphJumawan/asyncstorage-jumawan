c4dc4b1eff254ed95e4f27018be421c8bae8f24e
JessRaphJumawan <143191134+JessRaphJumawan@users.noreply.github.com>
2023-11-30 04:36:32 -0800

    Add files via upload

---
+++ Jumawan asyncstorage.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Simulate a login action
+    if (username === 'example' && password === 'password') {
+      alert('Login successful!');
+      // You can navigate to the next screen or perform any other action here
+    } else {
+      alert('Invalid credentials. Please try again.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+  },
+});
+
+export default LoginScreen;
